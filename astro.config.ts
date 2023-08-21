f4fb08d214a21e883c7d9814d9e8bbc448ecd0a4
Thuc Tran <kienthuc4701@gmail.com>
2023-08-21 16:26:35 -0400

    add home page

--- astro.config.ts
+++ astro.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   },
   experimental: {
     viewTransitions: true,
+    assets: true
   },
 });
--- uno.config.ts
+++ uno.config.ts
@@ -5,10 +5,14 @@ import {
   transformerDirectives,
   presetAttributify,
 } from "unocss";
-
 export default defineConfig({
+  rules: [
+      ['service-shadow', {
+        "box-shadow":'0 15px 30px -10px rgba(0, 0, 0, 0.2)'}]
+  ],
   shortcuts: [
-    { "i-logo": "i-logos-astro w-6em h-6em transform transition-800" },
+    { "i-logo": "i-logos-astro w-6em h-6em transform transition-800",
+  },
   ],
   transformers: [transformerDirectives()],
   presets: [
@@ -24,9 +28,11 @@ export default defineConfig({
   preflights: [
     {
       getCSS: ({ theme }) => `
+      body {
+        background:#fff;
+      }
       .nav-links {
         display:none;
-        display: unset;
       }
       .expanded {
         display:unset!important;
