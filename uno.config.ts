49ff6d0fe301a4369f2a38b5f642c8ba25482bd1
Thuc Tran <kienthuc4701@gmail.com>
2023-08-22 18:52:13 -0400

    change home page

--- uno.config.ts
+++ uno.config.ts
@@ -7,8 +7,11 @@ import {
 } from "unocss";
 export default defineConfig({
   rules: [
+      ['bg-gray-light', {"background-color":'rgba(246, 231, 231, 0.2)'}],
       ['service-shadow', {
-        "box-shadow":'0 15px 30px -10px rgba(0, 0, 0, 0.2)'}]
+        "box-shadow":'0 15px 30px -10px rgba(0, 0, 0, 0.2)'}
+       ,],
+      ['bg-header', {'background': 'radial-gradient(circle, hsla(206, 97%, 49%, 1) 0%, hsla(201, 87%, 76%, 1) 100%)'}]
   ],
   shortcuts: [
     { "i-logo": "i-logos-astro w-6em h-6em transform transition-800",
@@ -28,6 +31,10 @@ export default defineConfig({
   preflights: [
     {
       getCSS: ({ theme }) => `
+      html.dark {
+        background-color: #0d0950;
+        color: #fff;
+      }
       body {
         background:#fff;
       }
